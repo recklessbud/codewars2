a89fe1bb4a874b0bd53b66d9a0ec9510d5a66bbf
Alexander <bureck400@gmail.com>
2023-10-20 21:56:32 +0000

    stringToArr

--- main.js
+++ main.js
@@ -1,2 +1,9 @@
 // alert('pp')
 //im up in this bitch again
+
+// Write a function to split a string and convert it into an array of words.
+  function stringToArr(string){
+     return string.split(' ')
+  }
+
+  console.log(stringToArr('i love you'))
