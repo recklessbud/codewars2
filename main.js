766005a6e257660fd61b3423354a3d1fc5971c90
Alexander <alexanderglover478@gmail.com>
2023-10-31 21:41:57 +0000

    getthing the last even numbers from an arrat

--- main.js
+++ main.js
@@ -710,11 +710,27 @@ how to order multiple strings of the same length. */
         }
 
 
-        console.log(findDp([4, 5, 6, 1, 3, 2, 2]))
+        // console.log(findDp([4, 5, 6, 1, 3, 2, 2]))
 
+/**Given an array of numbers, return a new array of length number containing the last even numbers from the original
+ *  array (in the same order). The original array will be not empty and will contain at least "number" even numbers.
+For example: */
 
+  function isEven(array, num){
+      const newArr = []
+        for (let index = 0; index < array.length; index++) {
+          const element = array[index];
+           if(element % 2 === 0){
+            newArr.push(element)
+           }else{
+            // newArr.push(index)
+           }
+          
+        }
+          return newArr.slice(-num)
+  }
 
-
+  console.log(isEven([1, 2, 3, 4, 5, 6, 7, 8, 9], 3));
 
 
 
